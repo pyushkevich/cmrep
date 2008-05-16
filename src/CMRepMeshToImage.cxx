8815632fa1f36f55277167a74d125e58854fb972
pauly2 <pauly2>
2008-05-16 18:02:31 +0000

    added minor utils

---
+++ src/CMRepMeshToImage.cxx
@@ -0,0 +1,279 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <vtkCellArray.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPointData.h>
+#include <vtkTriangleFilter.h>
+#include <vnl/vnl_matrix.h>
+#include <vnl/vnl_det.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+#include <itkImage.h>
+
+using namespace itk;
+using namespace std;
+
+#ifndef vtkFloatingPointType
+#define vtkFloatingPointType vtkFloatingPointType
+typedef float vtkFloatingPointType;
+#endif
+
+
+
+typedef vnl_vector_fixed<vtkFloatingPointType, 3> Vec;
+typedef itk::Image<float, 3> ImageType;
+  typedef itk::Image<unsigned char,3> ByteImage;
+
+void MapPoint(const Vec &x, ImageType *img, double *vout)
+{
+  ImageType::PointType pt;
+  pt[0] = x[0]; pt[1] = x[1]; pt[2] = x[2];
+  ContinuousIndex<double,3> idx;
+  img->TransformPhysicalPointToContinuousIndex(pt, idx);
+  vout[0] = idx[0]; vout[1] = idx[1]; vout[2] = idx[2];
+}
+
+void VertSet(double **vtx, double **X, size_t t, size_t i0, size_t i1, size_t i2, size_t side)
+{
+  if(side == 1)
+    {
+    vtx[3*t  ] = X[i0]; vtx[3*t+1] = X[i1]; vtx[3*t+2] = X[i2];
+    }
+  else
+    {
+    vtx[3*t  ] = X[i2]; vtx[3*t+1] = X[i1]; vtx[3*t+2] = X[i0];
+    }
+}
+
+void ScanTetrahedron(double *v1, double *v2, double *v3, double *v4, ByteImage *img)
+{
+  // Matrices for the point inside test
+  typedef vnl_matrix_fixed<double, 4, 4> Mat;
+  Mat D0, D1, D2, D3, D4;
+  D0.fill(1);
+  for(size_t col = 0; col < 3; col++)
+    {
+    D0(0,col) = v1[col]; 
+    D0(1,col) = v2[col]; 
+    D0(2,col) = v3[col]; 
+    D0(3,col) = v4[col]; 
+    }
+  D1 = D0; D2 = D0; D3 = D0; D4 = D0;
+  double det4 = vnl_det(D4);
+
+  // Get the image region to iterate
+  double xmin = D0.get_column(0).min_value(), xmax = D0.get_column(0).max_value();
+  double ymin = D0.get_column(1).min_value(), ymax = D0.get_column(1).max_value();
+  double zmin = D0.get_column(2).min_value(), zmax = D0.get_column(2).max_value();
+  ByteImage::IndexType idx;
+  idx[0] = (long) ceil(xmin); idx[1] = (long) ceil(ymin); idx[2] = (long) ceil(zmin);
+  ByteImage::SizeType sz;
+  sz[0] = (unsigned long) ( ceil(xmax) - ceil(xmin) );
+  sz[1] = (unsigned long) ( ceil(ymax) - ceil(ymin) );
+  sz[2] = (unsigned long) ( ceil(zmax) - ceil(zmin) );
+  ByteImage::RegionType rgn(idx, sz);
+  rgn.Crop(img->GetBufferedRegion());
+
+  // Iterate over the region
+  ImageRegionIteratorWithIndex<ByteImage> it(img, rgn);
+  for( ; !it.IsAtEnd(); ++it)
+    {
+    ByteImage::IndexType idx = it.GetIndex();
+    for(size_t col = 0; col < 3; col++)
+      {
+      D0(0,col) = idx[col];
+      D1(1,col) = idx[col];
+      D2(2,col) = idx[col];
+      D3(3,col) = idx[col];
+      }
+    double det0 = vnl_det(D0);
+    double det1 = vnl_det(D1);
+    double det2 = vnl_det(D2);
+    double det3 = vnl_det(D3);
+    if(det0 > 0 && det1 > 0 && det2 > 0 && det3 > 0 && det4 > 0)
+      it.Set(1);
+    else if(det0 < 0 && det1 < 0 && det2 < 0 && det3 < 0 && det4 < 0)
+      it.Set(1);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+  cout << "Usage: cmrep_fillmesh mesh.med.vtk reference.img output.img" << endl;
+  cout << "  creates a binary image from a cm-rep mesh " << endl;
+  if(argc < 4) return -1;
+
+  // Load the mesh and the input image
+  vtkPolyDataReader *reader = vtkPolyDataReader::New();
+  reader->SetFileName(argv[1]);
+  reader->Update();
+
+  vtkPolyData *mesh = reader->GetOutput();
+  typedef itk::Image<float, 3> ImageType;
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+  ReaderType::Pointer fltReader = ReaderType::New();
+  fltReader->SetFileName(argv[2]);
+  fltReader->Update();
+  ImageType::Pointer img = fltReader->GetOutput();
+
+  // Create a matching pair of images (increment and full)
+  ByteImage::Pointer iOut = ByteImage::New();
+  iOut->SetRegions(img->GetBufferedRegion());
+  iOut->SetOrigin(img->GetOrigin());
+  iOut->SetSpacing(img->GetSpacing());
+  iOut->Allocate();
+  iOut->FillBuffer(0);
+
+  ByteImage::Pointer iTmp = ByteImage::New();
+  iTmp->SetRegions(iOut->GetBufferedRegion());
+  iTmp->Allocate();
+  iTmp->FillBuffer(0);
+
+  // Get the resolution
+  int res[3];
+  res[0] = iOut->GetBufferedRegion().GetSize()[0];
+  res[1] = iOut->GetBufferedRegion().GetSize()[1];
+  res[2] = iOut->GetBufferedRegion().GetSize()[2];
+
+  // Create an array of vertices for each 'wedge'
+  double **vtx = new double *[42];
+  double **X = new double*[10];
+  for(size_t q = 0; q < 10; q++)
+    X[q] = new double[3];
+
+  // Get the spoke arrays, radius
+  vtkDataArray *spoke[2], *rad;
+  spoke[0] = mesh->GetPointData()->GetArray("Spoke1");
+  spoke[1] = mesh->GetPointData()->GetArray("Spoke2");
+  rad = mesh->GetPointData()->GetArray("Radius Function");
+
+  if(spoke[0] == NULL || spoke[1] == NULL || rad == NULL)
+    {
+    cerr << "Spoke/radius arrays missing in mesh" << endl;
+    return -1;
+    }
+
+  vtkPolyData *dump = vtkPolyData::New();
+  vtkPoints *dumppts = vtkPoints::New();
+  vtkIntArray *dumparr = vtkIntArray::New();
+  dumparr->SetName("wedge");
+  dump->Allocate();
+  dump->SetPoints(dumppts);
+  dump->GetPointData()->AddArray(dumparr);
+
+  // Iterate over the cells in the image
+  size_t nt = mesh->GetNumberOfPolys();
+  vtkCellArray *tri = mesh->GetPolys();
+  vtkIdType npts; vtkIdType *pts;
+  typedef vnl_vector_fixed<vtkFloatingPointType, 3> Vec;
+  size_t itri = 0;
+  size_t iwedge = 0;
+  for(tri->InitTraversal(); tri->GetNextCell(npts,pts); itri++)
+    {
+    for(size_t side = 0; side < 2; side++)
+      {
+      // Get the vertices
+      Vec x[3], b[3], f[3];
+      for(size_t i=0;i<3;i++)
+        {
+        x[i] = Vec(mesh->GetPoint(pts[i]));
+        Vec spk(spoke[side]->GetTuple(pts[i]));
+        b[i] = x[i] + spk;
+        }
+
+      // Add the vertices to each of the wedge's quad faces
+      Vec q[3];
+      q[0] = 0.25 * (x[1] + b[1] + x[2] + b[2]);
+      q[1] = 0.25 * (x[2] + b[2] + x[0] + b[0]);
+      q[2] = 0.25 * (x[0] + b[0] + x[1] + b[1]);
+      Vec C = (q[0] + q[1] + q[2]) / 3.0;
+
+
+      // Convert to image coordinates
+      MapPoint(x[0], img, X[0]); MapPoint(x[1], img, X[1]); MapPoint(x[2], img, X[2]);
+      MapPoint(b[0], img, X[3]); MapPoint(b[1], img, X[4]); MapPoint(b[2], img, X[5]);
+      MapPoint(q[0], img, X[6]); MapPoint(q[1], img, X[7]); MapPoint(q[2], img, X[8]);
+      MapPoint(C, img, X[9]);
+
+      ScanTetrahedron(X[0], X[1], X[2], X[9], iOut);
+      ScanTetrahedron(X[3], X[5], X[4], X[9], iOut);
+
+      ScanTetrahedron(X[0], X[8], X[1], X[9], iOut);
+      ScanTetrahedron(X[1], X[8], X[4], X[9], iOut);
+      ScanTetrahedron(X[4], X[8], X[3], X[9], iOut);
+      ScanTetrahedron(X[3], X[8], X[0], X[9], iOut);
+
+      ScanTetrahedron(X[1], X[6], X[2], X[9], iOut);
+      ScanTetrahedron(X[2], X[6], X[5], X[9], iOut);
+      ScanTetrahedron(X[5], X[6], X[4], X[9], iOut);
+      ScanTetrahedron(X[4], X[6], X[1], X[9], iOut);
+
+      ScanTetrahedron(X[2], X[7], X[0], X[9], iOut);
+      ScanTetrahedron(X[0], X[7], X[3], X[9], iOut);
+      ScanTetrahedron(X[3], X[7], X[5], X[9], iOut);
+      ScanTetrahedron(X[5], X[7], X[2], X[9], iOut);
+
+      // Create an array of faces
+      VertSet(vtx, X, 0, 0, 1, 2, side);
+      VertSet(vtx, X, 1, 3, 5, 4, side);
+
+      VertSet(vtx, X, 2, 0, 8, 1, side);
+      VertSet(vtx, X, 3, 1, 8, 4, side);
+      VertSet(vtx, X, 4, 4, 8, 3, side);
+      VertSet(vtx, X, 5, 3, 8, 0, side);
+
+      VertSet(vtx, X, 6, 1, 6, 2, side);
+      VertSet(vtx, X, 7, 2, 6, 5, side);
+      VertSet(vtx, X, 8, 5, 6, 4, side);
+      VertSet(vtx, X, 9, 4, 6, 1, side);
+
+      VertSet(vtx, X,10, 2, 7, 0, side);
+      VertSet(vtx, X,11, 0, 7, 3, side);
+      VertSet(vtx, X,12, 3, 7, 5, side);
+      VertSet(vtx, X,13, 5, 7, 2, side);
+
+      for(size_t r = 0; r < 14; r++)
+        {
+        vtkIdType ids[3];
+        ids[0] = dump->GetPoints()->InsertNextPoint(vtx[r*3+0]);
+        ids[1] = dump->GetPoints()->InsertNextPoint(vtx[r*3+1]);
+        ids[2] = dump->GetPoints()->InsertNextPoint(vtx[r*3+2]);
+        dump->InsertNextCell(VTK_TRIANGLE, 3, ids);
+        dumparr->InsertNextTuple1(iwedge);
+        dumparr->InsertNextTuple1(iwedge);
+        dumparr->InsertNextTuple1(iwedge);
+        }
+      iwedge++;
+
+      // for(size_t r= 0; r<42;r++)
+      //  cout << vtx[r][0] << " " << vtx[r][1] << " "  << vtx[r][2] << endl;
+
+      // drawBinaryTrianglesSheetFilled(iOut->GetBufferPointer(), res, vtx, 14);
+      // drawBinaryTrianglesFilled(iOut->GetBufferPointer(), res, vtx, 14);
+      // unsigned char *ptOut = iOut->GetBufferPointer();
+      // unsigned char *ptTmp = iTmp->GetBufferPointer();
+      // for(size_t j = 0; j < iTmp->GetBufferedRegion().GetNumberOfPixels(); j++)
+      //   {
+      //  if(ptTmp[j] > 0)
+      //    {
+      //    ptOut[j] = 1; ptTmp[j] = 0;
+      //    }
+      //  }
+      }
+      cout << "." << flush;
+      if((itri + 1) % 80 == 0)
+        cout << " " << itri+1 << "/" << nt << endl;
+    }
+
+  cout << endl;
+
+  typedef itk::ImageFileWriter<ByteImage> WriterType;
+  WriterType::Pointer fltWrite= WriterType::New();
+  fltWrite->SetFileName(argv[3]);
+  fltWrite->SetInput(iOut);
+  fltWrite->Update();
+}
---
+++ src/CMRepToWarp.cxx
@@ -0,0 +1,346 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include <vtkCellArray.h>
+#include <vtkPolyData.h>
+#include <vtkPointData.h>
+#include <vtkPolyDataReader.h>
+#include <vtkTriangleFilter.h>
+#include <vnl/vnl_matrix.h>
+#include <vnl/vnl_det.h>
+#include <vnl/vnl_inverse.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+#include <itkImage.h>
+
+using namespace itk;
+using namespace std;
+
+#ifndef vtkFloatingPointType
+#define vtkFloatingPointType vtkFloatingPointType
+typedef float vtkFloatingPointType;
+#endif
+
+
+
+typedef vnl_vector_fixed<vtkFloatingPointType, 3> Vec;
+typedef itk::Image<float, 3> ImageType;
+  typedef itk::Image<unsigned char,3> ByteImage;
+
+void MapPoint(const Vec &x, ImageType *img, Vec &vout)
+{
+  ImageType::PointType pt;
+  pt[0] = x[0]; pt[1] = x[1]; pt[2] = x[2];
+  ContinuousIndex<double,3> idx;
+  img->TransformPhysicalPointToContinuousIndex(pt, idx);
+  vout[0] = idx[0]; vout[1] = idx[1]; vout[2] = idx[2];
+}
+
+void VertSet(double **vtx, double **X, size_t t, size_t i0, size_t i1, size_t i2, size_t side)
+{
+  if(side == 1)
+    {
+    vtx[3*t  ] = X[i0]; vtx[3*t+1] = X[i1]; vtx[3*t+2] = X[i2];
+    }
+  else
+    {
+    vtx[3*t  ] = X[i2]; vtx[3*t+1] = X[i1]; vtx[3*t+2] = X[i0];
+    }
+}
+
+void ScanTetrahedron(Vec *X, Vec *V, size_t i1, size_t i2, size_t i3, size_t i4, ImageType::Pointer warp[3])
+{
+  // Matrices for the point inside test
+  typedef vnl_matrix_fixed<double, 4, 4> Mat;
+  Mat D0, D1, D2, D3, D4;
+  D0.fill(1);
+  for(size_t col = 0; col < 3; col++)
+    {
+    D0(0,col) = X[i1][col]; 
+    D0(1,col) = X[i2][col]; 
+    D0(2,col) = X[i3][col]; 
+    D0(3,col) = X[i4][col]; 
+    }
+  D1 = D0; D2 = D0; D3 = D0; D4 = D0;
+  double det4 = vnl_det(D4);
+
+  // Get the image region to iterate
+  double xmin = D0.get_column(0).min_value(), xmax = D0.get_column(0).max_value();
+  double ymin = D0.get_column(1).min_value(), ymax = D0.get_column(1).max_value();
+  double zmin = D0.get_column(2).min_value(), zmax = D0.get_column(2).max_value();
+  ImageType::IndexType idx;
+  idx[0] = (long) ceil(xmin); idx[1] = (long) ceil(ymin); idx[2] = (long) ceil(zmin);
+  ImageType::SizeType sz;
+  sz[0] = (unsigned long) ( ceil(xmax) - ceil(xmin) );
+  sz[1] = (unsigned long) ( ceil(ymax) - ceil(ymin) );
+  sz[2] = (unsigned long) ( ceil(zmax) - ceil(zmin) );
+  ImageType::RegionType rgn(idx, sz);
+
+  // Proceed only if region fits inside
+  if(rgn.Crop(warp[0]->GetBufferedRegion()))
+    {
+    // Set up interpolators for the deformation fields
+    vnl_matrix_fixed<double, 4, 3> VM, Q;
+    for(size_t col = 0; col < 3; col++)
+      {
+      VM[0][col] = V[i1][col];
+      VM[1][col] = V[i2][col];
+      VM[2][col] = V[i3][col];
+      VM[3][col] = V[i4][col];
+      }
+    Q = vnl_inverse(D4) * VM;
+
+    // Iterate over the region
+    ImageRegionIteratorWithIndex<ImageType> itx(warp[0], rgn);
+    ImageRegionIteratorWithIndex<ImageType> ity(warp[1], rgn);
+    ImageRegionIteratorWithIndex<ImageType> itz(warp[2], rgn);
+    for( ; !itx.IsAtEnd(); ++itx, ++ity, ++itz)
+      {
+      ByteImage::IndexType idx = itx.GetIndex();
+      for(size_t col = 0; col < 3; col++)
+        {
+        D0(0,col) = idx[col];
+        D1(1,col) = idx[col];
+        D2(2,col) = idx[col];
+        D3(3,col) = idx[col];
+        }
+      double det0 = vnl_det(D0);
+      double det1 = vnl_det(D1);
+      double det2 = vnl_det(D2);
+      double det3 = vnl_det(D3);
+      if(
+        (det0 > 0 && det1 > 0 && det2 > 0 && det3 > 0 && det4 > 0) ||
+        (det0 < 0 && det1 < 0 && det2 < 0 && det3 < 0 && det4 < 0))
+        {
+        // Interpolate the position of the voxel inside the tetrahedron
+        Vec v = D0.get_row(0) * Q;
+        itx.Set(v[0]);
+        ity.Set(v[1]);
+        itz.Set(v[2]);
+        }
+      }
+    }
+}
+
+int usage()
+{
+  cout << "cmrep_genwarp: " << endl;
+  cout << "  create a warp field between two images based on cm-rep normalization" << endl;
+  cout << "usage:" << endl;
+  cout << "  cmrep_to_warp [opts] cmrep1 cmrep2 ref_image warp_name" << endl;
+  cout << "parameters:" << endl;
+  cout << "  cmrep1, cmrep2  : VTK meshes (cmr_fit mesh dir, defX.med.vtk) " << endl;
+  cout << "  ref_image       : Image based on which warp field is evaluated" << endl;
+  cout << "options:" << endl;
+  cout << "  -e TEXT         : extension for warp files (def: nii.gz)" << endl;
+  cout << "  -x Xmax Xstep   : xi coord. sampling (def: 1 0.1)" << endl;
+  cout << "                    specifies whether the warp field extends past " << endl;
+  cout << "                    the model's boundary (i.e., 2 0.1). The step " << endl;
+  cout << "                    size affects the size of tetrahedra used to " << endl;
+  cout << "                    the warp field" << endl;
+  return -1;
+}
+
+int main(int argc, char *argv[])
+{
+  if(argc < 5) return usage();
+
+  // Get the main parameters
+  string fnwarp = argv[argc-1];
+  string fnimage = argv[argc-2];
+  string fncmrep2 = argv[argc-3];
+  string fncmrep1 = argv[argc-4];
+
+  // Get the options
+  string fnext = "nii.gz";
+  double xiMax = 1, xiStep = 0.1;
+
+  // Parse the options
+  for(size_t iopt = 1; iopt < argc-4; iopt++)
+    {
+    if(!strcmp(argv[iopt], "-e"))
+      {
+      fnext = argv[++iopt];
+      }
+    else if(!strcmp(argv[iopt], "-x"))
+      {
+      xiMax = atof(argv[++iopt]);
+      xiStep = atof(argv[++iopt]);
+      }
+    else
+      {
+      cerr << "Unknown option " << argv[iopt] << endl;
+      return usage();
+      }
+    }
+
+  // Load the two cm-rep meshes
+  vtkPolyDataReader *r1 = vtkPolyDataReader::New();
+  r1->SetFileName(fncmrep1.c_str());
+  r1->Update();
+  vtkPolyData *m1 = r1->GetOutput();
+
+  vtkPolyDataReader *r2 = vtkPolyDataReader::New();
+  r2->SetFileName(fncmrep2.c_str());
+  r2->Update();
+  vtkPolyData *m2 = r2->GetOutput();
+
+  // Read the reference image
+  typedef itk::Image<float, 3> ImageType;
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+  ReaderType::Pointer fltReader = ReaderType::New();
+  fltReader->SetFileName(fnimage.c_str());
+  fltReader->Update();
+  ImageType::Pointer img = fltReader->GetOutput();
+
+  // Create warp image (for now only the forward warp)
+  ImageType::Pointer warp[3], winverse[3];
+  for(size_t d = 0; d < 3; d++)
+    {
+    warp[d] = ImageType::New();
+    warp[d]->SetRegions(img->GetBufferedRegion());
+    warp[d]->SetOrigin(img->GetOrigin());
+    warp[d]->SetSpacing(img->GetSpacing());
+    warp[d]->Allocate();
+    warp[d]->FillBuffer(0.0);
+    }
+
+  // Get the resolution
+  int res[3];
+  res[0] = warp[0]->GetBufferedRegion().GetSize()[0];
+  res[1] = warp[0]->GetBufferedRegion().GetSize()[1];
+  res[2] = warp[0]->GetBufferedRegion().GetSize()[2];
+
+  // Get the spoke arrays, radius
+  vtkDataArray *spoke1[2], *spoke2[2], *rad1, *rad2;
+  spoke1[0] = m1->GetPointData()->GetArray("Spoke1");
+  spoke1[1] = m1->GetPointData()->GetArray("Spoke2");
+  rad1 = m1->GetPointData()->GetArray("Radius Function");
+  spoke2[0] = m2->GetPointData()->GetArray("Spoke1");
+  spoke2[1] = m2->GetPointData()->GetArray("Spoke2");
+  rad2 = m2->GetPointData()->GetArray("Radius Function");
+
+  if(spoke1[0] == NULL || spoke1[1] == NULL || rad1 == NULL)
+    {
+    cerr << "Spoke/radius arrays missing in cmrep1" << endl;
+    return -1;
+    }
+  if(spoke2[0] == NULL || spoke2[1] == NULL || rad2 == NULL)
+    {
+    cerr << "Spoke/radius arrays missing in cmrep2" << endl;
+    return -1;
+    }
+  if(m1->GetNumberOfPoints() != m2->GetNumberOfPoints())
+    {
+    cerr << "CM-rep meshes have different number of points " << endl;
+    return -1;
+    }
+  if(m1->GetNumberOfPolys() != m2->GetNumberOfPolys())
+    {
+    cerr << "CM-rep meshes have different number of triangles " << endl;
+    return -1;
+    }
+
+  // Iterate over the cells in the image
+  size_t nt = m1->GetNumberOfPolys();
+  vtkCellArray *tri = m1->GetPolys();
+  vtkIdType npts; vtkIdType *pts;
+  typedef vnl_vector_fixed<vtkFloatingPointType, 3> Vec;
+  size_t itri = 0;
+  size_t iwedge = 0;
+  for(tri->InitTraversal(); tri->GetNextCell(npts,pts); itri++)
+    {
+    for(size_t side = 0; side < 2; side++)
+      {
+      // Get the medial vertices and the spokes
+      Vec x1[3], s1[3];
+      Vec x2[3], s2[3];
+      for(size_t i=0;i<3;i++)
+        {
+        x1[i] = Vec(m1->GetPoint(pts[i]));
+        x2[i] = Vec(m2->GetPoint(pts[i]));
+        s1[i] = Vec(spoke1[side]->GetTuple(pts[i]));
+        s2[i] = Vec(spoke2[side]->GetTuple(pts[i]));
+        }
+
+      // Loop over the wedges
+      for(double xi = 0; xi < xiMax; xi += xiStep)
+        {
+        
+        // Get the extents of the wedge in each cm-rep
+        Vec a1[3], b1[3], a2[3], b2[3];
+        for(size_t i=0;i<3;i++)
+          {
+          a1[i] = x1[i] + xi * s1[i];
+          a2[i] = x2[i] + xi * s2[i];
+          b1[i] = x1[i] + (xi + xiStep) * s1[i];
+          b2[i] = x2[i] + (xi + xiStep) * s2[i];
+          }
+
+        // Break quad faces of wedges by inserting vertices
+        Vec q1[3], q2[3];
+        q1[0] = 0.25 * (a1[1] + b1[1] + a1[2] + b1[2]);
+        q1[1] = 0.25 * (a1[2] + b1[2] + a1[0] + b1[0]);
+        q1[2] = 0.25 * (a1[0] + b1[0] + a1[1] + b1[1]);
+        q2[0] = 0.25 * (a2[1] + b2[1] + a2[2] + b2[2]);
+        q2[1] = 0.25 * (a2[2] + b2[2] + a2[0] + b2[0]);
+        q2[2] = 0.25 * (a2[0] + b2[0] + a2[1] + b2[1]);
+
+        // Compute centroid of each wedge
+        Vec C1 = (q1[0] + q1[1] + q1[2]) / 3.0;
+        Vec C2 = (q2[0] + q2[1] + q2[2]) / 3.0;
+
+        // Convert cm-rep1 to image coordinates
+        Vec X[10], V[10];
+        MapPoint(a1[0], img, X[0]); MapPoint(a1[1], img, X[1]); MapPoint(a1[2], img, X[2]);
+        MapPoint(b1[0], img, X[3]); MapPoint(b1[1], img, X[4]); MapPoint(b1[2], img, X[5]);
+        MapPoint(q1[0], img, X[6]); MapPoint(q1[1], img, X[7]); MapPoint(q1[2], img, X[8]);
+        MapPoint(C1, img, X[9]);
+
+        // For each of these ten vertices, compute the displacement
+        V[0] = a2[0] - a1[0]; V[1] = a2[1] - a1[1]; V[2] = a2[2] - a1[2];
+        V[3] = b2[0] - b1[0]; V[4] = b2[1] - b1[1]; V[5] = b2[2] - b1[2];
+        V[6] = q2[0] - q1[0]; V[7] = q2[1] - q1[1]; V[8] = q2[2] - q1[2];
+        V[9] = C2-C1;
+
+        // Interpolate the warp for each tetrahedron 
+        ScanTetrahedron(X, V, 0, 1, 2, 9, warp);
+        ScanTetrahedron(X, V, 3, 5, 4, 9, warp);
+
+        ScanTetrahedron(X, V, 0, 8, 1, 9, warp);
+        ScanTetrahedron(X, V, 1, 8, 4, 9, warp);
+        ScanTetrahedron(X, V, 4, 8, 3, 9, warp);
+        ScanTetrahedron(X, V, 3, 8, 0, 9, warp);
+
+        ScanTetrahedron(X, V, 1, 6, 2, 9, warp);
+        ScanTetrahedron(X, V, 2, 6, 5, 9, warp);
+        ScanTetrahedron(X, V, 5, 6, 4, 9, warp);
+        ScanTetrahedron(X, V, 4, 6, 1, 9, warp);
+
+        ScanTetrahedron(X, V, 2, 7, 0, 9, warp);
+        ScanTetrahedron(X, V, 0, 7, 3, 9, warp);
+        ScanTetrahedron(X, V, 3, 7, 5, 9, warp);
+        ScanTetrahedron(X, V, 5, 7, 2, 9, warp);
+        }
+      }
+      cout << "." << flush;
+      if((itri + 1) % 80 == 0)
+        cout << " " << itri+1 << "/" << nt << endl;
+    }
+
+  // Write the warp fields out
+  string suffix = "xvec." + fnext;
+  for(size_t d = 0; d < 3; d++)
+    {
+    suffix[0] = d + 'x';
+    string fname = fnwarp + suffix;
+
+    typedef itk::ImageFileWriter<ImageType> WriterType;
+    WriterType::Pointer fltWrite= WriterType::New();
+    fltWrite->SetFileName(fname.c_str());
+    fltWrite->SetInput(warp[d]);
+    fltWrite->Update();
+    }
+}
