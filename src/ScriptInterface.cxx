ad4c01ed6af33e6b93dfb4e18814af806c37fccc
pyushkevich <pyushkevich>
2014-12-08 15:01:20 +0000

    Added symmetric closest point code to parameters?

--- src/ScriptInterface.cxx
+++ src/ScriptInterface.cxx
@@ -697,6 +697,10 @@ void MedialPDE::ConfigureEnergyTerms(
       }
       break;
 
+    case OptimizationParameters::CLOSEST_POINT:
+      xTermImage = new SymmetricClosestPointMatchTerm(xMedialModel, image, 32);
+      break;
+
     default:
       throw MedialModelException("Unknown energy term specified");
     }
