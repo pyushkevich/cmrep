fb2da300374581d24450573ed7797159c9b43dd1
Paul Yushkevich <pyushkevich@gmail.com>
2021-03-26 14:17:08 +0000

    changed to new cdash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,13 +1,15 @@
 ## This file should be placed in the root directory of your project.
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
-## # The following are required to uses Dart and the Cdash dashboard
+##
+## # The following are required to submit to the CDash dashboard:
 ##   ENABLE_TESTING()
 ##   INCLUDE(CTest)
+
 set(CTEST_PROJECT_NAME "cmrep")
-set(CTEST_NIGHTLY_START_TIME "04:05:00 EST")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "itksnap.org")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=cmrep")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=cmrep")
 set(CTEST_DROP_SITE_CDASH TRUE)
